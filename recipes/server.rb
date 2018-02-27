346b571aa51127adaf9a2c49e1148ae50c1aa170
Cloud User <centos@ip-172-31-1-78.us-west-1.compute.internal>
2018-02-27 17:44:23 +0000

    Added Geralt image downlaod to server.rb and to HTML template

--- recipes/server.rb
+++ recipes/server.rb
@@ -2,13 +2,13 @@ package 'httpd' do
    action :install
 end
 
-cookbook_file '/var/www/html/index.html' do
-   source 'index.html'
+remote_file '/var/www/html/geralt.png' do
+   source 'https://vignette.wikia.nocookie.net/witcher/images/a/a0/Tw3_Geralt_of_Rivia_newest_render.png/revision/latest?cb=20170606064646'
 end
 
-#template 'var/www/html/index.html' do
-#   source 'index.html.erb'
-#end
+template 'var/www/html/index.html' do
+   source 'index.html.erb'
+end
 
 service 'httpd' do
    action [ :enable, :start]
