ab7beeea319298a22881755a0b372c490eac82e3
Cloud User <centos@ip-172-31-1-78.us-west-1.compute.internal>
2018-02-27 16:59:55 +0000

    changed html file in server.rb to be a static page and not a dynamic template

--- recipes/server.rb
+++ recipes/server.rb
@@ -2,12 +2,13 @@ package 'httpd' do
    action :install
 end
 
-template '/var/www/html/index.html' do
-   source 'index.html.erb'
-   variables(
-      :name => 'John Doe'
-   )
+cookbook_file '/var/www/html/index.html' do
+   source 'index.html'
 end
+
+#template 'var/www/html/index.html' do
+#   source 'index.html.erb'
+#end
 
 service 'httpd' do
    action [ :enable, :start]
